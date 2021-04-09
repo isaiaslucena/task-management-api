cd5b9dc2d4fda9bb36d596580e2f3fcd3b6d9cfe
isaiasneto <isaiasneto@gmail.com>
2021-04-09 15:34:29 -0300

    set user param

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -4,19 +4,21 @@ import { TaskStatus } from "./task-status.enum";
 import { Task } from "./task.entity";
 import { Like } from 'typeorm';
 import { GetTasksFilterDto } from "./dto/get-tasks-filter.do";
+import { User } from "src/auth/user.entity";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
-  async getTaskWithFilters(filterDto: GetTasksFilterDto): Promise<Task[]> {
+  async getTaskWithFilters(filterDto: GetTasksFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
 
     const w: {
       status?: string;
+      user: any;
       where?: [
         { title?: any; },
         { description?: any; }
       ]
-    } = {};
+    } = { user };
 
     if (status) w.status = status;
 
@@ -29,13 +31,14 @@ export class TaskRepository extends Repository<Task> {
     return result;
   }
 
-  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+  async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
     const { title, description } = createTaskDto;
 
     const task = new Task();
     task.title = title;
     task.description = description;
     task.status = TaskStatus.OPEN;
+    task.user = user;
     await task.save();
 
     return task;
