5275860b01fcb6b341775728674de76061f4819f
isaiasneto <isaiasneto@gmail.com>
2021-04-09 15:32:45 -0300

    created auth dto

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,14 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MinLength(3)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(6)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, { message: "password too weak" })
+  password: string;
+}
