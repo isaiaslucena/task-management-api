59e8c80ac9fa94ee8d952f49fa8d1ade0be83ea9
isaiasneto <isaiasneto@gmail.com>
2021-04-09 15:32:19 -0300

    created auth repository

---
+++ src/auth/auth.repository.ts
@@ -0,0 +1,39 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
+import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
+import { User } from "./user.entity";
+import * as bcrypt from "bcrypt";
+
+@EntityRepository(User)
+export class UserRepository extends Repository<User>  {
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return await bcrypt.hash(password, salt);
+  }
+
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const salt = await bcrypt.genSalt();
+
+    const user = new User();
+    user.username = username;
+    user.salt = salt;
+    user.password = await this.hashPassword(password, salt);
+
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === '23505') throw new ConflictException(error.detail);
+      throw new InternalServerErrorException(error.detail);
+    }
+  }
+
+  async validateUserPassword(authCredentialsDto: AuthCredentialsDto): Promise<boolean> {
+    const { username, password } = authCredentialsDto;
+
+    const user = await this.findOne({ username });
+
+    const isPasswordValid = await user.isPasswordValid(password);
+    return user && isPasswordValid
+  }
+}
